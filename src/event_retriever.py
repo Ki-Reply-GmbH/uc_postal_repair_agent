e0f4ae10b82e214ba147f281734b0fceb8a48a13
Timo Kubera <timokubera@yahoo.com>
2024-02-21 16:26:53 +0100

    Write events to file for further investigatioN

--- src/event_retriever.py
+++ src/event_retriever.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import urllib.parse
 
 class EventRetriever:
     def __init__(self, token_id):
@@ -18,6 +19,26 @@ class EventRetriever:
                                 +'/requests?sorting=newest', headers=headers)
         for event in response.json()["data"]:
             self.events.append(event)
+
+    def write_events_to_file(self, filename):
+        # Create a new list to hold the prettified events
+        prettified_events = []
+
+        # Iterate over the events
+        for event in self.events:
+            # Copy the event so we don't modify the original
+            prettified_event = event.copy()
+
+            # Prettify the 'content' field
+            content = urllib.parse.unquote(prettified_event['content'])
+            prettified_event['content'] = json.loads(content.replace('payload=', ''))
+
+            # Add the prettified event to the new list
+            prettified_events.append(prettified_event)
+
+        # Write the prettified events to the file
+        with open(filename, 'w') as f:
+            json.dump(prettified_events, f, indent=4)
 
     def __str__(self):
         s = ""
