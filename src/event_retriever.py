f175f3e88c961ab48715d2b96f3deeb568b4ca12
Timo Kubera <timokubera@yahoo.com>
2024-02-21 16:11:16 +0100

    Retrieving webhook events

--- src/event_retriever.py
+++ src/event_retriever.py
@@ -2,9 +2,25 @@ import requests
 import json
 
 class EventRetriever:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, token_id):
+        self.token_id = token_id
+        self.events = []
+
+        self.get_events()
 
     def get_events(self):
-        response = requests.get(self.url)
-        return json.loads(response.text)
+        headers = {"api-key": self.token_id}
+        print("URL:")
+        print("https://webhook.site/token/" + self.token_id + "/requests?sorting=newest")
+        print()
+        response = requests.get('https://webhook.site/token/'
+                                + self.token_id 
+                                +'/requests?sorting=newest', headers=headers)
+        for event in response.json()["data"]:
+            self.events.append(event)
+
+    def __str__(self):
+        s = ""
+        for event in self.events:
+            s += json.dumps(event, indent=4)
+        return s
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from event_retriever import EventRetriever
 
-events = EventRetriever("https://webhook.site/733cf8c8-554f-453c-8da2-8dffd77d349c").get_events()
+events = EventRetriever("733cf8c8-554f-453c-8da2-8dffd77d349c")
 
 print(events)
