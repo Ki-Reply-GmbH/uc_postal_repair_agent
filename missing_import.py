79ce5b84f7ec502c711d0e2ae481c2bb7256ffd2
Timo Kubera <timokubera@yahoo.com>
2024-02-23 09:30:21 +0100

    Moving broken script to different repo

--- missing_import.py
+++
@@ -1,9 +0,0 @@
-"""
-This code will intentionally raise a NameError because the numpy module is not
-installed. It's used to create an error in the GitHub Actions Pipeline.
-"""
-
-vector1 = np.array([1, 2, 3])
-vector2 = np.array([4, 5, 6])
-
-result = np.add(vector1, vector2)
