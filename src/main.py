0f26d15ebb0a3f9abb1403adbe5d831b5105ff60
Timo Kubera <t.kubera@reply.de>
2024-03-10 13:08:27 +0100

    Refactoring main.py

--- src/main.py
+++ src/main.py
@@ -7,66 +7,67 @@ from utils.cache import DisabledCache
 from config import Config, PromptConfig
 from models import LLModel
 
-events = json.loads(open("failed_check_runs.json").read())
+def main():
+    # Arguments
+    git_user = os.environ["GIT_USERNAME"]
+    token = os.environ["GIT_ACCESS_TOKEN"]
 
-# Arguments
-git_user = os.environ["GIT_USERNAME"]
-token = os.environ["GIT_ACCESS_TOKEN"]
+    # Information extracted from webhook
+    #TODO Mit Azure EventHub extrahieren
+    owner = "TimoKubera"
+    repo = "broken_webhooks"
+    branch = "main"
 
-# Information extracted from webhook
-owner = "TimoKubera"
-repo = "broken_webhooks"
-branch = "main"
-
-# Initialize GitHandler
-gh = GitHandler()
-gh.initialize(
-    branch,
-    git_user,
-    owner,
-    token,
-    repo
+    # Initialize GitHandler
+    gh = GitHandler()
+    gh.initialize(
+        branch,
+        git_user,
+        owner,
+        token,
+        repo
     )
-gh.clean_up()
-gh.clone()
+    gh.clean_up()
+    gh.clone()
 
-# Initialize RepairAgent
-infos = EventHandler(events[0])
-ag = RepairAgent(
-    PromptConfig(),
-    LLModel(
-        Config(),
-        DisabledCache(".")
+    # Load events
+    events = json.loads(open("failed_check_runs.json").read())
+
+    # Initialize RepairAgent
+    infos = EventHandler(events[0])
+    ag = RepairAgent(
+        PromptConfig(),
+        LLModel(
+            Config(),
+            DisabledCache(".")
         ),
-    infos.get_log()
+        infos.get_log()
     )
 
-print("Log:\n", ag._failed_log)
-print("Task:")
-print(ag._tasks)
-
-file_path = ag.find_file(
-    gh.get_tmp_path(),
-    ag.get_f_name(),
-    ag.get_error_area()
+    # Find file and repair
+    file_path = ag.find_file(
+        gh.get_tmp_path(),
+        ag.get_f_name(),
+        ag.get_error_area()
     )
-print(file_path)
+    ag.repair_file(file_path)
 
-ag.repair_file(file_path)
-print("Response:\n", ag.get_response())
-
-# Korrigierte Datei schreiben, commiten und pushen
-gh.write_responses(
-    [file_path],
-    [ag.get_response()]
+    # Write responses, commit and push
+    gh.write_responses(
+        [file_path],
+        [ag.get_response()]
     )
-commit_msg = ag.make_commit_msg()
-gh.commit_and_push(
-    [file_path],
-    commit_msg
-)
+    commit_msg = ag.make_commit_msg()
+    gh.commit_and_push(
+        [file_path],
+        commit_msg
+    )
 
-gh.create_pull_request(
-    "Repairing broken CI/CD pipeline",
-    commit_msg
-)
+    # Create pull request
+    gh.create_pull_request(
+        "Repairing broken CI/CD pipeline",
+        commit_msg
+    )
+
+if __name__ == "__main__":
+    main()
